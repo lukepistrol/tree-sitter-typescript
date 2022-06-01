e1f353d9f51e5584146d5472fe490607332f47cb
Lukas Pistrol <lukas@pistrol.com>
2022-06-01 13:56:31 +0200

    add spm support

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterTS",
+    products: [
+        .library(name: "TreeSitterTS", targets: ["TreeSitterTS"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterTS",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "grammar.js",
+                    "LICENSE",
+                    "Makefile",
+                    "package.json",
+                    "README.md",
+                    "script",
+                    "tsx/src/grammar.json",
+                    "tsx/src/node-types.json",
+                    "typescript/src/grammar.json",
+                    "typescript/src/node-types.json",
+                ],
+                sources: [
+                    "tsx/src/parser.c",
+                    "tsx/src/scanner.c",
+                    "typescript/src/parser.c",
+                    "typescript/src/scanner.c",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+)
